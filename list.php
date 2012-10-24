1d082dfcade5ee511ba4f204f50005126e88667a
Vasanth Mohan <vmohan7@corn22.stanford.edu>
2012-10-23 22:57:22 -0700

    move files and then basics

---
+++ list.php
@@ -0,0 +1,13 @@
+<head>
+	<?php
+		include 'head.php';
+	?>
+</head>
+<div class="ui-grid-a">
+	<div class="ui-block-a">
+		<a href="description.php">Apple</a>
+	</div>
+	<div class="ui-block-b"> 
+		<img src="images/apple.jpg" />
+	</div>
+</div>
