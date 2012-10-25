be9d729647b9d04d8c3dced9796b405ea1aa83ed
Vasanth Mohan <vmohan7@corn16.stanford.edu>
2012-10-25 15:53:30 -0700

    Switch to index temp

--- description.php
+++ description.php
@@ -11,7 +11,7 @@
 
 	<img src="images/apple.jpg" />
 	
-	<form action="submit.php" method="post">
+	<form action="index.php" method="post">
 		<h1> Apple </h1> <input type="hidden" name="name" value="Apple" />
 		Quantity: <input type="text" name="quantity" value= "1" style="width: 25%;"/><br />
 		<input type="submit" value="Submit" />
