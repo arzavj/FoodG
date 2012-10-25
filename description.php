1a94bc916bf1dea12e491168730fe97c0de8a06c
Arzav Jain <arzavj@stanford.edu>
2012-10-25 16:07:50 -0700

    changed apple form layout

--- description.php
+++ description.php
@@ -1,23 +1,31 @@
-<head>
-        <?php
-                include 'head.php';
-        ?>
-</head>
-<div class="ui-grid-a">
-	<div class="ui-block-a"><img src="images/apple.jpg" /></div>
-	<div class="ui-block-b">
-		<form action="submit.php" method="post">
-			<h1> Apple </h1> <input type="hidden" name="name" value="Apple" />
-			<div data-role="fieldcontain">
-					<label for="quantity" class="ui-input-text">Quantity: </label>
-					<input type="number" name="quantity" value= "1" style="width: 25%;"/><span>units</span>
-			<div data-role="fieldcontain">
-					<label for="expiry" class="ui-input-text">Expiry Date: </label>
-					<input type="date" name="expiry"></input>			
+<html>
+	<head>
+	        <?php
+	                include 'head.php';
+	        ?>
+	</head>
+	<body>
+		<div data-role="header">
+            <a href="list.php" data-icon="back">Back</a>
+			<h1>Apple</h1>
+	    </div><!-- /header -->
+		<div class="ui-grid-a">
+			<div class="ui-block-a"><img src="images/apple.jpg" /></div>
+			<div class="ui-block-b">
+				<form action="submit.php" method="post">
+				 	<input type="hidden" name="name" value="Apple" />
+					<div data-role="fieldcontain">
+							<label for="quantity" class="ui-input-text">Quantity: </label>
+							<input type="number" name="quantity" value= "1" style="width: 25%;"/><span>units</span>
+					<div data-role="fieldcontain">
+							<label for="expiry" class="ui-input-text">Expiry Date: </label>
+							<input type="date" name="expiry"></input>			
+					</div>
+					<input type="submit" data-theme="b" value="Submit" />
+				</form>
 			</div>
-			<input type="submit" value="Submit" />
-		</form>
-	</div>
-</div><!-- /grid-a -->
+		</div><!-- /grid-a -->
+	</body>
+</html>
 
 
