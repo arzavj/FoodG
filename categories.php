bd7c11ed3135efc53dc03f94fc9f5668583d8b8d
Vasanth Mohan <vmohan7@corn16.stanford.edu>
2012-10-25 15:19:42 -0700

    Link fruits

--- categories.php
+++ categories.php
@@ -17,7 +17,7 @@
 		<ul data-role="listview">
 			<li><a href="#">Meats</a></li>
 			<li><a href="#">Vegetables</a></li>
-			<li><a href="#">Fruits</a></li>
+			<li><a href="list.php" rel="external">Fruits</a></li>
 			<li><a href="#">Dairy</a></li>
 			<li><a href="#">Fish & Seafood</a></li>
 			<li><a href="#">Beverages</a></li>
