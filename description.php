14183fe21206aad5eba9d15c20a54217911f849b
Vasanth Mohan <vmohan7@corn16.stanford.edu>
2012-10-25 15:27:16 -0700

    Headers

--- description.php
+++ description.php
@@ -3,12 +3,17 @@
                 include 'head.php';
         ?>
 </head>
+<body>
+        <div data-role="header">
+                <a href="#Home" data-icon="back">Back</a>
+                        <h1>Categories</h1>
+        </div><!-- /header -->
 
-<img src="images/apple.jpg" />
-
-<form action="submit.php" method="post">
-	<h1> Apple </h1> <input type="hidden" name="name" value="Apple" />
-	Quantity: <input type="text" name="quantity" value= "1" style="width: 25%;"/><br />
-	<input type="submit" value="Submit" />
-</form>
-
+	<img src="images/apple.jpg" />
+	
+	<form action="submit.php" method="post">
+		<h1> Apple </h1> <input type="hidden" name="name" value="Apple" />
+		Quantity: <input type="text" name="quantity" value= "1" style="width: 25%;"/><br />
+		<input type="submit" value="Submit" />
+	</form>
+</body>
--- list.php
+++ list.php
@@ -3,11 +3,17 @@
 		include 'head.php';
 	?>
 </head>
-<div class="ui-grid-a">
-	<div class="ui-block-a">
-		<a href="description.php">Apple</a>
+<body>
+        <div data-role="header">
+                <a href="#Home" data-icon="back">Back</a>
+                        <h1>Categories</h1>
+        </div><!-- /header -->
+	<div class="ui-grid-a">
+		<div class="ui-block-a">
+			<a href="description.php" rel="external" >Apple</a>
+		</div>
+		<div class="ui-block-b"> 
+			<img src="images/apple.jpg" />
+		</div>
 	</div>
-	<div class="ui-block-b"> 
-		<img src="images/apple.jpg" />
-	</div>
-</div>
+</body>
