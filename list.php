4d8320669c683ca1a1808dceb044628cdf03e16e
Vasanth Mohan <vmohan7@corn16.stanford.edu>
2012-10-25 15:45:29 -0700

    Style changes

--- list.php
+++ list.php
@@ -8,6 +8,18 @@
                 <a href="#Home" data-icon="back">Back</a>
                         <h1>Categories</h1>
         </div><!-- /header -->
+	
+	<ul data-role="listview">
+		<li>
+		<div data-role="navbar">
+			<ul>
+				<li><img src="images/apple.jpg" class="thumb"></li>
+				<li><a href="description.php" rel="external" >Apple</a></li>
+			</ul>
+		</div>
+		</li>
+	</ul>
+<!--
 	<div class="ui-grid-a">
 		<div class="ui-block-a">
 			<a href="description.php" rel="external" >Apple</a>
@@ -16,4 +28,6 @@
 			<img src="images/apple.jpg" />
 		</div>
 	</div>
+
+-->
 </body>
