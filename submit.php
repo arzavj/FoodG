4fba89c0e52cd8535a185925dcd500d211a00ee2
Vasanth Mohan <vmohan7@cardinal2.stanford.edu>
2012-11-05 14:55:33 -0800

    Update and make storage and food unique

--- submit.php
+++ submit.php
@@ -4,9 +4,16 @@
 	$user_storage_query = sprintf("SELECT user_storages.id AS id from user_storages inner join users ON (users.id = user_storages.user_id) WHERE users.username = \"%s\"", $_COOKIE["username"]);
 	$user_storage = mysql_fetch_array(mysql_query($user_storage_query));
 
+	
 	if ($_POST["update"] == "0"){ //Not an update
-		$query = sprintf("INSERT INTO user_foods(user_storage_id, food_id, quantity) VALUES (%s, %s, %s)", $user_storage["id"], $_POST["food_id"], $_POST["quantity"]);
-		mysql_query($query);
+		$test = sprintf("SELECT * from user_foods WHERE user_storage_id = %s AND food_id = %s", $user_storage["id"], $_POST["food_id"]);
+		$test_result = mysql_query($test);
+		if (mysql_num_rows($test_result) == 0){
+			$query = sprintf("INSERT INTO user_foods(user_storage_id, food_id, quantity) VALUES (%s, %s, %s)", $user_storage["id"], $_POST["food_id"], $_POST["quantity"]);
+			mysql_query($query);
+		} else{
+			//TODO make the alert and update points
+		}
 	} else{ //Need to update
 		if($_POST["btnS"] == "Update"){
 			$query = sprintf("UPDATE user_foods SET quantity = %s WHERE user_storage_id = %s AND food_id = %s", $_POST["quantity"], $user_storage["id"], $_POST["food_id"]);
@@ -15,9 +22,8 @@
 			$query = sprintf("DELETE FROM user_foods WHERE user_storage_id = %s AND food_id = %s", $user_storage["id"], $_POST["food_id"]);
 			mysql_query($query);
 		}
-
-
 	}
+
 ?>
 
 <html>
