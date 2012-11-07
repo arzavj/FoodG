3310cfdd276d0f2651154e71b29c2d002a209556
Arzav Jain <arzavj@stanford.edu>
2012-11-06 16:51:19 -0800

    changed phpmyadmin password to foodg

--- config.php
+++ config.php
@@ -1,4 +1,4 @@
 <?php
-	$link = mysql_connect('mysql-user-master.stanford.edu', 'ccs147vmohan7', 'athauxoh');
+	$link = mysql_connect('mysql-user-master.stanford.edu', 'ccs147vmohan7', 'foodg');
 	mysql_select_db('c_cs147_vmohan7');
 ?>
--- submit.php
+++ submit.php
@@ -5,21 +5,31 @@
 	$user_storage = mysql_fetch_array(mysql_query($user_storage_query));
 
 	
-	if ($_POST["update"] == "0"){ //Not an update
+	if ($_POST["update"] == "0") //if new item is added through add item button
+	{ 
 		$test = sprintf("SELECT * from user_foods WHERE user_storage_id = %s AND food_id = %s", $user_storage["id"], $_POST["food_id"]);
 		$test_result = mysql_query($test);
-		if (mysql_num_rows($test_result) == 0){
+		if (mysql_num_rows($test_result) == 0) //if item does not exist in the fridge
+		{
 			$query = sprintf("INSERT INTO user_foods(user_storage_id, food_id, quantity, quantity_type_id) VALUES (%s, %s, %s, %s)", $user_storage["id"], $_POST["food_id"], $_POST["quantity"], $_POST["quantity_type_id"]);
 			mysql_query($query);
-			updateFridgeVolume($user_storage["id"], calculateAddedVolume($_POST["food_id"], $_POST["quantity"], $_POST["quantity_type_id"]));
-		} else{
+			updateFridgeVolume($user_storage["id"], calculateAddedVolume($_POST["food_id"], $_POST["quantity"], $_POST["quantity_type_id"])); //this line updates the curr_volume column in the database appropriately
+		} 
+		else //if item already exists in the fridge
+		{
 			//TODO make the alert and update points
 		}
-	} else{ //Need to update
-		if($_POST["btnS"] == "Update"){
+	} 
+	else //if food item is clicked on through home page and is updated or removed all
+	{ 
+		if($_POST["btnS"] == "Update")
+		{
+			$query = sprintf("SELECT  from user_foods WHERE id = %s", $food_id);
 			$query = sprintf("UPDATE user_foods SET quantity = %s, quantity_type_id = %s WHERE user_storage_id = %s AND food_id = %s", $_POST["quantity"], $_POST["quantity_type_id"], $user_storage["id"], $_POST["food_id"]);
 			mysql_query($query);
-		} else if ($_POST["btnS"] == "Remove All"){
+		} 
+		else if ($_POST["btnS"] == "Remove All")
+		{
 			$query = sprintf("DELETE FROM user_foods WHERE user_storage_id = %s AND food_id = %s", $user_storage["id"], $_POST["food_id"]);
 			mysql_query($query);
 		}
@@ -28,11 +38,11 @@
 	//returns added volume
 	function calculateAddedVolume($food_id, $quantity, $quantity_type_id)
 	{
-		$query = sprintf("SELECT * from quantity_types WHERE id = %s", $quantity_type_id);
+		$query = sprintf("SELECT quantity_types.kg_equivalent from quantity_types WHERE id = %s", $quantity_type_id);
 		$quantityRow = mysql_fetch_array(mysql_query($query));
 		$query = sprintf("SELECT * from foods WHERE id = %s", $food_id);
 		$foodRow = mysql_fetch_array(mysql_query($query));
-		if($quantityRow["kg_equivalent"]==0)
+		if($quantityRow["kg_equivalent"]==0) //in the case of units where we need the weight of 1 unit from the foods table
 		{
 			//take care of things like apples
 		}
