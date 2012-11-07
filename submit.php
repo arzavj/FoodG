33c812e1e2e38b6b035e0c6aeecf7f706c8b33e0
Vasanth Mohan <vmohan7@corn29.stanford.edu>
2012-11-07 15:19:08 -0800

    if 0

--- submit.php
+++ submit.php
@@ -9,7 +9,7 @@
 	{ 
 		$test = sprintf("SELECT * from user_foods WHERE user_storage_id = %s AND food_id = %s", $user_storage["id"], $_POST["food_id"]);
 		$test_result = mysql_query($test);
-		if (mysql_num_rows($test_result) == 0) //if item does not exist in the fridge
+		if (mysql_num_rows($test_result) == 0 && intval($_POST["quantity"]) > 0) //if item does not exist in the fridge
 		{
 			$query = sprintf("INSERT INTO user_foods(user_storage_id, food_id, quantity, quantity_type_id) VALUES (%s, %s, %s, %s)", $user_storage["id"], $_POST["food_id"], $_POST["quantity"], $_POST["quantity_type_id"]);
 			mysql_query($query);
@@ -22,13 +22,13 @@
 	} 
 	else //if food item is clicked on through home page and is updated or removed all
 	{ 
-		if($_POST["btnS"] == "Update")
+		if($_POST["btnS"] == "Update" && intval($_POST["quantity"]) > 0)
 		{
 			$query = sprintf("SELECT  from user_foods WHERE id = %s", $food_id);
 			$query = sprintf("UPDATE user_foods SET quantity = %s, quantity_type_id = %s WHERE user_storage_id = %s AND food_id = %s", $_POST["quantity"], $_POST["quantity_type_id"], $user_storage["id"], $_POST["food_id"]);
 			mysql_query($query);
 		} 
-		else if ($_POST["btnS"] == "Remove All")
+		else if ($_POST["btnS"] == "Remove All" || $_POST["quantity"] == "0")
 		{
 			$query = sprintf("DELETE FROM user_foods WHERE user_storage_id = %s AND food_id = %s", $user_storage["id"], $_POST["food_id"]);
 			mysql_query($query);
