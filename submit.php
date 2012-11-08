f0373cae19834834164dec00c257199e0bd1d637
Arzav Jain <arzavj@stanford.edu>
2012-11-07 23:02:25 -0800

    finished with updating volume of fridge

--- submit.php
+++ submit.php
@@ -12,7 +12,7 @@
 		{
 			$query = sprintf("INSERT INTO user_foods(user_storage_id, food_id, quantity, quantity_type_id) VALUES (%s, %s, %s, %s)", $user_storage["id"], $_POST["food_id"], $_POST["quantity"], $_POST["quantity_type_id"]);
 			mysql_query($query);
-			updateFridgeVolume($user_storage["id"], calculateAddedVolume($_POST["food_id"], $_POST["quantity"], $_POST["quantity_type_id"])); //this line updates the curr_volume column in the database appropriately
+			updateFridgeVolume($user_storage["id"], calculateVolume($_POST["food_id"], $_POST["quantity"], $_POST["quantity_type_id"])); //this line updates the curr_volume column in the database appropriately
 		} 
 		else //if item already exists in the fridge
 		{
@@ -21,45 +21,38 @@
 	} 
 	else //if food item is clicked on through home page and is updated or removed all
 	{ 
+		$foodInFridge = mysql_fetch_array($test_result);
+		$foodsOldVolume = calculateVolume($_POST["food_id"], $foodInFridge["quantity"], $foodInFridge["quantity_type_id"]);
+		$foodsNewVolume = 0;
 		if($_POST["btnS"] == "Update" && intval($_POST["quantity"]) > 0)
 		{
-			if(intval($_POST["quantity_type_id"])==$test_result["quantity_type_id"]) //if user enters same units as before
-			{
-				$changeInQuantity = intval($_POST["quantity"]) - $test_result["quantity"]; //change = new - old
-				updateFridgeVolume($user_storage["id"], calculateAddedVolume($_POST["food_id"], $changeInQuantity, $_POST["quantity_type_id"]));
-			}		
-			else
-			{
-				//in case user enters different units then when previously entered
-			}
+			$foodsNewVolume = calculateVolume($_POST["food_id"], $_POST["quantity"], $_POST["quantity_type_id"]);
 			$query = sprintf("UPDATE user_foods SET quantity = %s, quantity_type_id = %s WHERE user_storage_id = %s AND food_id = %s", $_POST["quantity"], $_POST["quantity_type_id"], $user_storage["id"], $_POST["food_id"]);
 			mysql_query($query);
 		} 
 		else if ($_POST["btnS"] == "Remove All" || $_POST["quantity"] == "0")
 		{
-			//change fridge volume
 			$query = sprintf("DELETE FROM user_foods WHERE user_storage_id = %s AND food_id = %s", $user_storage["id"], $_POST["food_id"]);
 			mysql_query($query);
 		}
+		updateFridgeVolume($user_storage["id"], $foodsNewVolume - $foodsOldVolume);
 	}
 	
 	//returns added volume
-	function calculateAddedVolume($food_id, $quantity, $quantity_type_id)
+	function calculateVolume($food_id, $quantity, $quantity_type_id)
 	{
 		$query = sprintf("SELECT quantity_types.kg_equivalent from quantity_types WHERE id = %s", $quantity_type_id);
 		$quantityRow = mysql_fetch_array(mysql_query($query));
 		$query = sprintf("SELECT * from foods WHERE id = %s", $food_id);
 		$foodRow = mysql_fetch_array(mysql_query($query));
 		if($quantityRow["kg_equivalent"]==0) //in the case of units where we need the weight of 1 unit from the foods table
-		{
-			//take care of things like apples
-		}
+			return ($quantity*$foodRow["weight_per_unit"])/$foodRow["density"];
 		else
 			return ($quantity*$quantityRow["kg_equivalent"])/$foodRow["density"]; //volume = mass (kg) / density (kg/m3)
 	}
 	
 	//gets current volume and updates it
-	//addedVolume is a double
+	//addedVolume is a double. addedVolume is change in volume
 	function updateFridgeVolume($user_storage_id, $addedVolume)
 	{
 		$query = sprintf("SELECT user_storages.curr_volume from user_storages WHERE id = %s", $user_storage_id);
