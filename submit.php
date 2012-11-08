1f452f27e2bedc24d84005ececd34ad63de22d8e
Arzav Jain <arzavj@stanford.edu>
2012-11-07 17:41:56 -0800

    pulling changes to cookies using user ids instead from vasanth

--- submit.php
+++ submit.php
@@ -4,11 +4,10 @@
 	$user_storage_query = sprintf("SELECT user_storages.id AS id from user_storages inner join users ON (users.id = user_storages.user_id) WHERE users.username = \"%s\"", $_COOKIE["username"]);
 	$user_storage = mysql_fetch_array(mysql_query($user_storage_query));
 
-	
+	$test = sprintf("SELECT * from user_foods WHERE user_storage_id = %s AND food_id = %s", $user_storage["id"], $_POST["food_id"]);
+	$test_result = mysql_query($test);
 	if ($_POST["update"] == "0") //if new item is added through add item button
 	{ 
-		$test = sprintf("SELECT * from user_foods WHERE user_storage_id = %s AND food_id = %s", $user_storage["id"], $_POST["food_id"]);
-		$test_result = mysql_query($test);
 		if (mysql_num_rows($test_result) == 0) //if item does not exist in the fridge
 		{
 			$query = sprintf("INSERT INTO user_foods(user_storage_id, food_id, quantity, quantity_type_id) VALUES (%s, %s, %s, %s)", $user_storage["id"], $_POST["food_id"], $_POST["quantity"], $_POST["quantity_type_id"]);
@@ -24,12 +23,21 @@
 	{ 
 		if($_POST["btnS"] == "Update")
 		{
-			$query = sprintf("SELECT  from user_foods WHERE id = %s", $food_id);
+			if(intval($_POST["quantity_type_id"])==$test_result["quantity_type_id"]) //if user enters same units as before
+			{
+				$changeInQuantity = intval($_POST["quantity"]) - $test_result["quantity"]; //change = new - old
+				updateFridgeVolume($user_storage["id"], calculateAddedVolume($_POST["food_id"], $changeInQuantity, $_POST["quantity_type_id"]));
+			}		
+			else
+			{
+				//in case user enters different units then when previously entered
+			}
 			$query = sprintf("UPDATE user_foods SET quantity = %s, quantity_type_id = %s WHERE user_storage_id = %s AND food_id = %s", $_POST["quantity"], $_POST["quantity_type_id"], $user_storage["id"], $_POST["food_id"]);
 			mysql_query($query);
 		} 
 		else if ($_POST["btnS"] == "Remove All")
 		{
+			//change fridge volume
 			$query = sprintf("DELETE FROM user_foods WHERE user_storage_id = %s AND food_id = %s", $user_storage["id"], $_POST["food_id"]);
 			mysql_query($query);
 		}
