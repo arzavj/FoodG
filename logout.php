920cd46f3972c899a184f89b1de00eb9df692d11
Vasanth Mohan <vmohan7@corn22.stanford.edu>
2012-11-09 09:34:55 -0800

    use empty quotes instead

--- logout.php
+++ logout.php
@@ -1,6 +1,6 @@
 <?php
 	setcookie('user-id', '', time()-60*60*24*365);
-	setcookie("shop-cart-mode",false,time() + (86400 * 1));
-	setcookie("cart",array(), time() + (86400 * 1));
+	setcookie("shop-cart-mode",'',time() + (86400 * 1));
+	setcookie("cart",'', time() + (86400 * 1));
 	include "loginForm.php";
 ?>
