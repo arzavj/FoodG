653aeb6d29995de45582454f1780bb5686147a10
Vasanth Mohan <vmohan7@corn28.stanford.edu>
2012-11-08 23:01:06 -0800

    Updates to show cart

--- myCart.php
+++ myCart.php
@@ -11,7 +11,20 @@
 			</div>
 			<div data-role="content">
 				<?php
-					echo $_COOKIE["shop-cart-mode"];
+					$array = json_decode($_COOKIE["cart"], true);
+					if(!is_null($array) && $_COOKIE["shop-cart-mode"]==true){
+						foreach ($array as $hash){
+							$map = json_decode($hash, true);
+							$item = mysql_fetch_assoc(mysql_query(sprintf("SELECT * from foods WHERE id = %s", $map["food_id"])));
+							echo "<div>\n";
+							$link = sprintf("<a href='description.php?food=%s&update=1'>", $item['id']);
+							echo $link;
+							echo "<p>".$item['food'];
+							$image = sprintf("<img src= %s class = %s />", $item['image_url'],'thumb' );
+							echo $image."</a></p>\n";
+							echo "</div>\n";
+						}
+					}
 				?>
 			</div>
 	</body>
