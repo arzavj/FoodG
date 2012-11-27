74957a665b0d5f0adcd8ce28d4c863925fb12786
Vasanth Mohan <vmohan7@corn27.stanford.edu>
2012-11-27 11:24:58 -0800

    Change tp use category images

--- ajaxCategory.php
+++ ajaxCategory.php
@@ -4,17 +4,19 @@
 	if ($_POST["catID"] != "0"){
 		$cat = mysql_query(sprintf("SELECT category from categories WHERE id = %s", $_POST["catID"]));
 		$cat = mysql_fetch_array($cat);
-		$query = sprintf("SELECT *, foods.id AS fID from ((user_foods inner join foods ON foods.id = user_foods.food_id) 
+		$query = sprintf("SELECT *, foods.id AS fID from (((user_foods inner join foods ON foods.id = user_foods.food_id) 
 			inner join quantity_types ON quantity_types.id = user_foods.quantity_type_id) 
-			inner join user_storages ON user_storages.id = user_foods.user_storage_id 
+			inner join user_storages ON user_storages.id = user_foods.user_storage_id)
+			inner join categories ON categories.id = foods.category_id
 			WHERE foods.category_id = %s AND user_storages.user_id = %s", $_POST["catID"], $_COOKIE['user-id']);
 ?>
 		<!--<div style="text-align:center;"><h3><?= $cat["category"] ?></h3></div>-->
 <?php
 	} else{
-		$query = sprintf("SELECT *, foods.id AS fID from ((user_foods inner join foods ON foods.id = user_foods.food_id) 
+		$query = sprintf("SELECT *, foods.id AS fID from (((user_foods inner join foods ON foods.id = user_foods.food_id) 
 			inner join quantity_types ON quantity_types.id = user_foods.quantity_type_id) 
-			inner join user_storages ON user_storages.id = user_foods.user_storage_id 
+			inner join user_storages ON user_storages.id = user_foods.user_storage_id)
+			inner join categories ON categories.id = foods.category_id
 			WHERE user_storages.user_id = %s", $_COOKIE['user-id']);
 	}
 
@@ -33,7 +35,7 @@
 	?>
 				<div class="ui-block-a">
 					<a href='description.php?food=<?= $item['fID']?>&update=1'>
-					 <img src= "<?= $item['image_url'] ?>" class = 'thumb' /> 
+					 <img src= "<?= $item['display_url'] ?>" class = 'thumb' /> 
 					</a>
 					<div class="imgCaption"><?= $item['food'] ?></div>
 				</div>
@@ -43,7 +45,7 @@
 	?>
 				<div class="ui-block-b">
 					<a href='description.php?food=<?= $item['fID']?>&update=1'>
-					 <img src= "<?= $item['image_url'] ?>" class = 'thumb' /> 
+					 <img src= "<?= $item['display_url'] ?>" class = 'thumb' /> 
 					</a>
 					<div class="imgCaption"><?= $item['food'] ?></div>
 				</div>
@@ -53,7 +55,7 @@
 	?>
 				<div class="ui-block-c">
 					<a href='description.php?food=<?= $item['fID']?>&update=1'>
-					 <img src= "<?= $item['image_url'] ?>" class = 'thumb' /> 
+					 <img src= "<?= $item['display_url'] ?>" class = 'thumb' /> 
 					</a>
 					<div class="imgCaption"><?= $item['food'] ?></div>
 				</div>
@@ -63,7 +65,7 @@
 	?>
 				<div class="ui-block-d">
 					<a href='description.php?food=<?= $item['fID']?>&update=1'>
-					 <img src= "<?= $item['image_url'] ?>" class = 'thumb' /> 
+					 <img src= "<?= $item['display_url'] ?>" class = 'thumb' /> 
 					</a>
 					<div class="imgCaption"><?= $item['food'] ?></div>
 				</div>
